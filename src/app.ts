fd72bffcde0c0f21cb8f54278e296972315d45dd
anucha-tk <anuchataowkaen@gmail.com>
2025-11-29 12:15:52 +0700

    fix(app): fix app

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,33 @@
+import { cors } from "@elysiajs/cors";
 import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 import { config } from "./common/config";
 import { db } from "./common/db";
 import { handleError } from "./common/error/handler";
 import { loggerPlugin } from "./common/logger/loggerPlugin";
+import { NotFoundResponse } from "./common/response/ApiResponse";
 import { routeV1 } from "./routes/v1";
+
 export const app = new Elysia();
 
 db;
 
 app
+	.use(
+		cors({
+			origin: "http://localhost:3001",
+			methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+			credentials: true,
+			allowedHeaders: ["Content-Type", "Authorization"],
+		}),
+	)
 	.use(openapi())
 	.use(loggerPlugin)
 	.onError(handleError)
+	.all("*", ({ set }) => {
+		set.status = 404;
+		return new NotFoundResponse().send();
+	})
 	.use(routeV1)
 	.listen(config.app.port, () => {
 		console.log(`Environment: ${config.app.env}`);
